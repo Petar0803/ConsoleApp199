b72634e84889a5204525a4a1bec446df4526fded
Seslio <svetlozar.temelkov.pp26@pgmtplovdiv.info>
2026-01-13 12:50:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void PrintSign(int number)
+    {
+        if (number > 0)
+        {
+            Console.WriteLine($"The number {number} is positive.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine($"The number {number} is negative.");
+        }
+        else
+        {
+            Console.WriteLine($"The number {number} is zero.");
+        }
+    }
+
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        PrintSign(number);
+    }
+}
